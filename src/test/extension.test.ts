154bd1aa10759e0ab54abcad53d08929e7dc6348
Joseph Larson <larson.joseph@gmail.com>
2018-03-20 15:24:09 -0600

    Add formatting test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,10 +1,28 @@
-import { workspace } from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import * as assert from 'assert';
+import { commands, window, workspace } from 'vscode';
 
 suite('Extension Tests', function() {
     test('extension settings are registered', function() {
         assert(Number.isInteger(workspace.getConfiguration().get('black.lineLength') as number));
         assert(typeof workspace.getConfiguration().get('black.fast') === 'boolean');
+    });
+
+    test('successfully formats documents', async function() {
+        this.timeout(10000);
+        const fileString = fs.readFileSync(path.join('src', 'test', 'unformatted_test.py'), 'utf8');
+        const doc = await workspace.openTextDocument({ language: 'python', content: fileString });
+        const unformatted = doc.getText();
+        const preLength = doc.lineCount;
+        await window.showTextDocument(doc);
+        await commands.executeCommand('editor.action.formatDocument');
+        const formatted = doc.getText();
+        const postLength = doc.lineCount;
+
+        // test that the output is different than the input and that it is longer, line-wise
+        assert.notEqual(unformatted, formatted);
+        assert(postLength > preLength);
     });
 });
---
+++ src/test/unformatted_test.py
@@ -0,0 +1,58 @@
+def func_no_args():
+  a; b; c
+  if True: raise RuntimeError
+  if False: ...
+  for i in range(10):
+    print(i)
+    continue
+  return None
+async def coroutine(arg):
+ "Single-line docstring. Multiline is harder to reformat."
+ async with some_connection() as conn:
+     await conn.do_what_i_mean('SELECT bobby, tables FROM xkcd', timeout=2)
+ await asyncio.sleep(1)
+@asyncio.coroutine
+@some_decorator(
+with_args=True,
+many_args=[1,2,3]
+)
+def function_signature_stress_test(number:int,no_annotation=None,text:str="default",* ,debug:bool=False,**kwargs) -> str:
+ return text[number:-1]
+def spaces(a=1, b=(), c=[], d={}, e=True, f=-1, g=1 if False else 2, h="", i=r''):
+ offset = attr.ib(default=attr.Factory( lambda: _r.uniform(10000, 200000)))
+ assert task._cancel_stack[:len(old_stack)] == old_stack
+def spaces2(result= _core.Value(None)):
+ ...
+def example(session):
+    result = session.query(models.Customer.id).filter(
+        models.Customer.account_id == account_id,
+        models.Customer.email == email_address,
+    ).order_by(
+        models.Customer.id.asc()
+    ).all()
+def long_lines():
+    if True:
+        typedargslist.extend(
+            gen_annotated_params(ast_args.kwonlyargs, ast_args.kw_defaults, parameters, implicit_default=True)
+        )
+    _type_comment_re = re.compile(
+        r"""
+        ^
+        [\t ]*
+        \#[ ]type:[ ]*
+        (?P<type>
+            [^#\t\n]+?
+        )
+        (?<!ignore)     # note: this will force the non-greedy + in <type> to match
+                        # a trailing space which is why we need the silliness below
+        (?<!ignore[ ]{1})(?<!ignore[ ]{2})(?<!ignore[ ]{3})(?<!ignore[ ]{4})
+        (?<!ignore[ ]{5})(?<!ignore[ ]{6})(?<!ignore[ ]{7})(?<!ignore[ ]{8})
+        (?<!ignore[ ]{9})(?<!ignore[ ]{10})
+        [\t ]*
+        (?P<nl>
+            (?:\#[^\n]*)?
+            \n?
+        )
+        $
+        """, re.MULTILINE | re.VERBOSE
+    )
